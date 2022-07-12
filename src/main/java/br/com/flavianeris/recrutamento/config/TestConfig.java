26878f64bc3f4f66f17b888c8b333fe279823e7f
unknown <mariannyneris2004@gmail.com>
2022-07-12 10:51:57 -0300

    Teste Config para as outras classes além de Candidato

--- src/main/java/br/com/flavianeris/recrutamento/config/TestConfig.java
+++ src/main/java/br/com/flavianeris/recrutamento/config/TestConfig.java
@@ -9,7 +9,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.flavianeris.recrutamento.entidades.Candidato;
+import br.com.flavianeris.recrutamento.entidades.CandidatoInfo;
+import br.com.flavianeris.recrutamento.entidades.Cargo;
+import br.com.flavianeris.recrutamento.entidades.Cliente;
+import br.com.flavianeris.recrutamento.repositorios.CandidatoInfoRepository;
 import br.com.flavianeris.recrutamento.repositorios.CandidatoRepository;
+import br.com.flavianeris.recrutamento.repositorios.CargoRepository;
+import br.com.flavianeris.recrutamento.repositorios.ClienteRepository;
 
 @Configuration
 @Profile("test")
@@ -17,6 +23,15 @@ public class TestConfig implements CommandLineRunner{
 
 	@Autowired
 	private CandidatoRepository candidatoRepository;
+	
+	@Autowired
+	private CargoRepository cargoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private CandidatoInfoRepository ciRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -33,5 +48,21 @@ public class TestConfig implements CommandLineRunner{
 				"wender@gmail.com", "teste", "ativo", "masculino");
 		
 		candidatoRepository.saveAll(Arrays.asList(candidato1, candidato2));
+		
+		Cargo cargo1 = new Cargo(null, "teste", "Teste Cargo 1", "curso 1, curso 2", "1234567");
+		
+		Cargo cargo2 = new Cargo(null, "teste 2", "Teste Cargo 2", "curso 3, curso 4", "7654321");
+		
+		cargoRepository.saveAll(Arrays.asList(cargo1, cargo2));
+		
+		Cliente cliente1 = new Cliente(null, "Flávia Neris", "Flávia", "1234567", 
+				"RH", "nenhuma", "nenhuma", "985104522", "flavia@gmail.com");
+		
+		clienteRepository.saveAll(Arrays.asList(cliente1));
+		
+		CandidatoInfo ci1 = new CandidatoInfo(null, null, "Formação", "2020", "2022",
+				"Ensino médio", null);
+		
+		ciRepository.saveAll(Arrays.asList(ci1));
 	}
 }
